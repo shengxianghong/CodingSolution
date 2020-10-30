5431c033829c706b9ae3c347941ed3963a14c38d
shengxianghong <1149996828@qq.com>
2020-10-30 17:32:24 +0800

    no message

---
+++ ThoughPhoto/ThoughPhoto/AppDelegate.m
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.m
+//  ThoughPhoto
+//
+//  Created by zkworld on 2020/10/30.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+
+@end
---
+++ ThoughPhoto/ThoughPhoto/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  ThoughPhoto
+//
+//  Created by zkworld on 2020/10/30.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
