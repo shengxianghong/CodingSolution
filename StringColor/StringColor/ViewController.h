7c2830b9020270fff7f1fd94635395dbb67d0b89
shengxianghong <1149996828@qq.com>
2020-11-13 17:28:00 +0800

    no message

---
+++ StringColor/StringColor/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  StringColor
+//
+//  Created by zkworld on 2020/11/10.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StringColor/StringColor/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  StringColor
+//
+//  Created by zkworld on 2020/11/10.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [UIColor colorNamed:@""];
+}
+
+
+@end
