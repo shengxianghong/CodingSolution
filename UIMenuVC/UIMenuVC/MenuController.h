ad089f5dab663efdf8038785909fcdd70ade5cdd
shengxianghong <1149996828@qq.com>
2020-11-20 17:37:44 +0800

    no message

---
+++ UIMenuVC/UIMenuVC/MenuController.h
@@ -0,0 +1,16 @@
+//
+//  MenuController.h
+//  UIMenuVC
+//
+//  Created by zkworld on 2020/11/19.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MenuController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ UIMenuVC/UIMenuVC/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  UIMenuVC
+//
+//  Created by zkworld on 2020/11/19.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
